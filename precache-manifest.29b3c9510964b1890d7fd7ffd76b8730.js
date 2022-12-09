2decdcdabfccef386b0faab89420317373952487
Yash Yadav <yashyadav3701@gmail.com>
2022-12-09 12:14:24 +0530

    Updates

---
+++ precache-manifest.29b3c9510964b1890d7fd7ffd76b8730.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "abb45dd23c934b1fbcbe",
+    "url": "/my-app/static/css/main.3dc06c2e.chunk.css"
+  },
+  {
+    "revision": "abb45dd23c934b1fbcbe",
+    "url": "/my-app/static/js/main.abb45dd2.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/my-app/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/my-app/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "99b7f12dffb4a4f3de0c",
+    "url": "/my-app/static/js/runtime~main.99b7f12d.js"
+  },
+  {
+    "revision": "6c00d7289bee43c3419ff377a44a3705",
+    "url": "/my-app/index.html"
+  }
+];
